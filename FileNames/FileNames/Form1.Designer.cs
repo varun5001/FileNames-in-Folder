7cbf0eefdb7aa8855c7a6d9aef8e87e06301788c
varun <sriram.varun672@gmail.com>
2015-03-22 13:28:05 +0530

    FileNamesinFolder
    This Application writes all file names in a selected folder to a CSV in
    selected folder

---
+++ FileNames/FileNames/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace FileNames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click on Browse to locate the folder for file names";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(282, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Click on Select to select destination folder to save CSV file";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 193);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Enter File Name, e.g., test.csv";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(355, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "If file already exists, data appends into the file else creates and writes data";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(94, 275);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Create/Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Click on Create/Update to Create/Update File";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(575, 320);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
+
---
+++ FileNames/FileNames/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileNames
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        DialogResult result;
+        List<String> fileNames = new List<string>();
+        String filePath;
+        public Form1()
+        {
+            InitializeComponent();
+            }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialog fbd = new FolderBrowserDialog();
+            result = fbd.ShowDialog();
+            //string[] filenames;
+            
+            string[] files = Directory.GetFiles(fbd.SelectedPath);
+            System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(Path.GetFileName(files[i]));
+                fileNames.Add(Path.GetFileName(files[i]));
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            result = fbd.ShowDialog();
+            filePath = fbd.SelectedPath;
+            //String fileName = textBox1.Text;
+            //Console.WriteLine(filePath);
+           // filePath = filePath + "\\" + fileName;
+           // Console.WriteLine(filePath);
+
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String file_Name = textBox1.Text;
+            Console.WriteLine(filePath);
+            filePath = filePath + "\\" + file_Name;
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+            var csv = new StringBuilder();
+            fileNames.ForEach(fileName =>
+            {
+                csv.AppendLine(string.Join(",", fileName));
+            });
+            File.WriteAllText("C:\\Users\\Varun\\documents\\visual studio 2013\\Projects\\FileNames\\FileNames\\FileNames.csv", csv.ToString());
+            File.WriteAllText(filePath, csv.ToString());
+            System.Windows.Forms.MessageBox.Show( textBox1.Text + " File Updated", "Message");
+            
+        }
+
+        }
+       
+
+        
+
+    }
+
